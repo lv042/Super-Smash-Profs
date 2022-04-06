e0d0683fedd62908bc01ccd29258c9012a1ae42f
ll071 <ll071@hdm-stuttgart.de>
2022-04-06 16:56:04 +0200

    Clearing

--- core/src/com/mygdx/pahu/IntroScreen.java
+++
@@ -1,7 +0,0 @@
-package com.mygdx.pahu;
-
-import com.badlogic.gdx.ScreenAdapter;
-
-public class IntroScreen extends ScreenAdapter {
-
-}
--- core/src/com/mygdx/pahu/physics.java
+++
@@ -1,5 +0,0 @@
-package com.mygdx.pahu;
-
-public class physics {
-
-}
