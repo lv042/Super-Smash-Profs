7b3bcdd535771817724597016fbe598d504ee105
lv042 <luca.von.kannen@hdm-stuttgart.de>
2022-04-13 18:03:08 +0200

    fixed scaling of IntroScreen.java for different devices

--- core/src/com/smashprofs/game/Screens/IntroScreen.java
+++ core/src/com/smashprofs/game/Screens/IntroScreen.java
@@ -9,14 +9,12 @@ import com.smashprofs.game.GameClass;
 import com.smashprofs.game.Helper.SoundManager;
 
 public class IntroScreen extends ScreenAdapter {
-    int w = 0;
-    int h = 0;
-    int tw = 0;
-    int th = 0;
+    int width = 1920;
+    int height = 1080;
 
     private SoundManager soundManager = SoundManager.getSoundManager_INSTANCE();
 
-    private float zoomFactor = 1.6f;
+    private float zoomFactor = 1f;
     OrthographicCamera camera = null;
     Texture texture = null;
     SpriteBatch batch = null;
@@ -30,14 +28,13 @@ public class IntroScreen extends ScreenAdapter {
 
 
         soundManager.setupMusic(introSong);
-        w = Gdx.graphics.getWidth();
-        h = Gdx.graphics.getHeight();
-        camera = new OrthographicCamera(w, h);
-        camera.position.set(w / 2, h / 2, 0); // Change the height --> h
+        width = Gdx.graphics.getWidth();
+        height = Gdx.graphics.getHeight();
+        camera = new OrthographicCamera(width, height);
+
         camera.update();
         texture = new Texture(Gdx.files.internal("logo.jpeg"));
-        tw = texture.getWidth();
-        th = texture.getHeight();
+
         batch = new SpriteBatch();
         this.game = game;
 
@@ -48,13 +45,13 @@ public class IntroScreen extends ScreenAdapter {
     public void render(float delta) {
 
         batch.setProjectionMatrix(camera.combined);
-        if(zoomFactor > 1.5f) zoomFactor -= 0.0001f;
+        if(zoomFactor > 0.935) zoomFactor -= 0.0005f;
         //System.out.println(zoomFactor);
         Gdx.gl.glClearColor(0, 0, 0, 1); //-> light blue
         //Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(Gdx.gl.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(texture, camera.position.x - (tw / 2) / zoomFactor, camera.position.y - (th / 2) / zoomFactor, tw / zoomFactor, th / zoomFactor);
+        batch.draw(texture, (camera.position.x / 2 - width / 2) / zoomFactor, (camera.position.y / 2 - height / 2) / zoomFactor, width / zoomFactor, height/ zoomFactor);
         batch.end();
 
         if (Gdx.input.isKeyJustPressed(com.badlogic.gdx.Input.Keys.ANY_KEY)) {
