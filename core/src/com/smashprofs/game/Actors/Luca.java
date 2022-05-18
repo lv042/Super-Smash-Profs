21251357167a7b476cf8c0f21219aa9caf20ea75
Alexander Bossert <ab306@hdm-stuttgart.de>
2022-05-18 16:32:13 +0200

    Added Luca class for different animations

---
+++ core/src/com/smashprofs/game/Actors/Luca.java
@@ -0,0 +1,14 @@
+package com.smashprofs.game.Actors;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class Luca extends Player {
+
+    public Luca(World world, InputState inputState, Vector2 spawnpoint, String playerName, String userData) {
+        super(world, inputState, spawnpoint, playerName, userData, new Texture("Sprites/Luca/luca_stand.png"), new Texture("Sprites/Luca/luca_run.png"), new Texture("Sprites/Alex_jump.png"));
+
+
+    }
+}
