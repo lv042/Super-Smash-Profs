b6781a43bf7b9598f011878ccff17afad8cdb507
ll071 <ll071@hdm-stuttgart.de>
2022-05-19 16:14:51 +0200

    Removed whitespace

--- core/src/com/smashprofs/game/Actors/Bullet.java
+++ core/src/com/smashprofs/game/Actors/Bullet.java
@@ -6,9 +6,6 @@ import com.badlogic.gdx.physics.box2d.*;
 public class Bullet extends Projectile{
 
 
-
-
-
     public Bullet(World world, Player playerOrigin, OrthographicCamera camera) {
         super(world, playerOrigin, "Bullet" ,  camera);
         userData = "Bullet";
