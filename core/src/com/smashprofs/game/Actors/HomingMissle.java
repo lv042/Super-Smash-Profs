0b1f1296953ec55b1ee829227e9662275213830a
ll071 <ll071@hdm-stuttgart.de>
2022-05-19 16:17:51 +0200

    Added super.update call in update

--- core/src/com/smashprofs/game/Actors/HomingMissle.java
+++ core/src/com/smashprofs/game/Actors/HomingMissle.java
@@ -22,6 +22,7 @@ public class HomingMissle extends Projectile{
 
     @Override
     public void update(float delta){
+        super.update(delta);
         Vector2 playerPos = targetPlayer.getPosition();
         Vector2 targetVector = new Vector2(playerPos.x - b2dbody.getPosition().x, playerPos.y - b2dbody.getPosition().y);
         b2dbody.setLinearVelocity(targetVector.nor().scl(0.4f));
