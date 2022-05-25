7080b1ad9e65f654b9c7b7f9d2629ae2ceeb71ef
Alexander Bossert <ab306@hdm-stuttgart.de>
2022-05-25 14:48:12 +0200

    Added Leo class

--- core/src/com/smashprofs/game/Actors/Luca.java
+++ core/src/com/smashprofs/game/Actors/Luca.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.physics.box2d.World;
 public class Luca extends Player {
 
     public Luca(World world, InputState inputState, Vector2 spawnpoint, String playerName, String userData) {
-        super(world, inputState, spawnpoint, playerName, userData, new Texture("Sprites/Luca/luca_stand.png"), new Texture("Sprites/Luca/luca_run.png"), new Texture("Sprites/Alex_jump.png"));
+        super(world, inputState, spawnpoint, playerName, userData, new Texture("Sprites/Luca/luca_stand.png"), new Texture("Sprites/Luca/luca_run.png"), new Texture("Sprites/Luca/luca_run.png"));
 
 
     }
