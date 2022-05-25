beb1df37cf89e92ed82d2841fccede52b446d443
ll071 <ll071@hdm-stuttgart.de>
2022-05-25 18:07:24 +0200

    Added Texture to super() call

--- core/src/com/smashprofs/game/Actors/Bullet.java
+++ core/src/com/smashprofs/game/Actors/Bullet.java
@@ -1,13 +1,14 @@
 package com.smashprofs.game.Actors;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.physics.box2d.*;
 
 public class Bullet extends Projectile{
 
 
-    public Bullet(World world, Player playerOrigin, OrthographicCamera camera) {
-        super(world, playerOrigin, "Bullet" ,  camera);
+    public Bullet(World world, Player playerOrigin) {
+        super(world, playerOrigin, "Bullet", new Texture("fireball.png"));
         userData = "Bullet";
     }
 
--- core/src/com/smashprofs/game/Actors/HomingMissle.java
+++ core/src/com/smashprofs/game/Actors/HomingMissle.java
@@ -1,6 +1,7 @@
 package com.smashprofs.game.Actors;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
 import com.smashprofs.game.Actors.Player;
@@ -9,8 +10,8 @@ public class HomingMissle extends Projectile{
     Vector2 targetVector = new Vector2();
     Player targetPlayer;
 
-    public HomingMissle(World world, Player playerOrigin, OrthographicCamera camera, Player playerTarget){
-        super(world, playerOrigin, "Bullet");
+    public HomingMissle(World world, Player playerOrigin, Player playerTarget){
+        super(world, playerOrigin, "Bullet", new Texture("missile.png"));
         b2dbody.setFixedRotation(false);
         b2dbody.setAngularVelocity(10);
         this.targetPlayer = playerTarget;
@@ -27,5 +28,6 @@ public class HomingMissle extends Projectile{
         Vector2 targetPlayPos = targetPlayer.getPosition();
         Vector2 targetVector = new Vector2(targetPlayPos.x - b2dbody.getPosition().x, targetPlayPos.y - b2dbody.getPosition().y);
         b2dbody.setLinearVelocity(targetVector.nor().scl(0.4f));
+
     }
 }
