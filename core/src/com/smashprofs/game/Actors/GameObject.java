444f79c0a41c4b5232e999fc9b6b96bace4251a9
ll071 <ll071@hdm-stuttgart.de>
2022-05-30 20:16:18 +0200

    Implemented general gravity for all GameObjects (Luca)

--- core/src/com/smashprofs/game/Actors/GameObject.java
+++ core/src/com/smashprofs/game/Actors/GameObject.java
@@ -1,5 +1,6 @@
 package com.smashprofs.game.Actors;
 import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 
 
@@ -7,6 +8,8 @@ public abstract class GameObject {
     public Body b2dbody;    // Can't be final because of the change in child classes
     public Sprite sprite;   // <>
     public String userData; // <> userData is a string to identify the object in the game
+    public float gravity = -0.098f;
+    public Vector2 gravityVector = new Vector2(0, gravity);
 
 
     public GameObject(String userData) {
