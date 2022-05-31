ae0e111d3fdc6089725e3d8349fa3cdb5c4ae1f7
ll071 <ll071@hdm-stuttgart.de>
2022-05-31 10:07:54 +0200

    Implemented rocketInertia

--- core/src/com/smashprofs/game/Actors/Projectiles/HomingMissile.java
+++ core/src/com/smashprofs/game/Actors/Projectiles/HomingMissile.java
@@ -7,13 +7,24 @@ import com.smashprofs.game.Actors.Players.Player;
 
 import java.util.Random;
 
+/**
+ * A missile that will follow its target
+ */
 public class HomingMissile extends Projectile {
 
     Vector2 targetVector = new Vector2();
     Player targetPlayer;
     Random rand = new Random();
 
+    /**
+     * Controls the flight speed of the missile
+     */
     float speed = 0.7f;
+
+    /**
+     * Controls how fast the missile will react to the movement of the targetPlayer
+     */
+    float missileInertia = 0.05f;
 
     public HomingMissile(World world, Player playerOrigin, Player playerTarget){
         super(world, playerOrigin, "Bullet" ,  new Texture("missile.png"), 10);
@@ -39,7 +50,8 @@ public class HomingMissile extends Projectile {
     public void update(float delta){
         super.update(delta);
         Vector2 tartgetPlayPos = targetPlayer.getPosition();
-        Vector2 targetVector = new Vector2(tartgetPlayPos.x - b2dbody.getPosition().x, tartgetPlayPos.y - b2dbody.getPosition().y);
+        Vector2 newTargetVector = new Vector2(tartgetPlayPos.x - b2dbody.getPosition().x, tartgetPlayPos.y - b2dbody.getPosition().y);
+        targetVector = targetVector.lerp(newTargetVector, missileInertia);
         b2dbody.setLinearVelocity(targetVector.nor().scl(0.5f));
         sprite.setRotation(targetVector.angleDeg());
     }
