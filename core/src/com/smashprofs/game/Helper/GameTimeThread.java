9401acb1e97799c173cec7a09874bf379b9cb789
Maurice Dolibois <md147@hdm-stuttgart.de>
2022-06-04 10:54:09 +0200

    added Gamethread

---
+++ core/src/com/smashprofs/game/Helper/GameTimeThread.java
@@ -0,0 +1,23 @@
+package com.smashprofs.game.Helper;
+
+
+public class GameTimeThread extends Thread{
+
+    @Override
+    public void run() {
+    gamePropertiesManager.edit(Keys.TIMESPLAYED,Integer.toString(Integer.parseInt(gamePropertiesManager.getEntry(Keys.TIMESPLAYED))+1));
+
+    while(this.isAlive()) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        String[] time= gamePropertiesManager.getEntry(Keys.GAMETIME).split(" ");
+        gamePropertiesManager.edit(Keys.GAMETIME,(Integer.parseInt(time[0])+1)+" sek");
+      }
+
+    }
+
+}
+
